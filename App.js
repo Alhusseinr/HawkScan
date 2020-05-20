392d82c5ebb5c0a6ef344034931bd7118b32b5d1
Rami Alhussein <Ramialhussein98@gmail.com>
2020-05-20 19:08:48 -0400

    ok

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
-import * as React from 'react';
+import React, { Component } from 'react';
 import Scan from './components/CameraScan'
 
-export default class App extends React.Component {
-
+export default class App extends Component {
     render() {
         return (
             <Scan />
